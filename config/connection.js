6abd5511ac1622727b0fc38457480ba461d57221
SBC <lochan.sharad@gmail.com>
2021-07-01 23:21:48 +0800

    connection updated with jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,7 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 
-// if (process.env.JAWSDB_URL) {
-
-//     sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-const sequelize = new Sequelize(
+const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -16,5 +12,5 @@ const sequelize = new Sequelize(
         port: 3306,
     }
 );
-// }
+
 module.exports = sequelize;
